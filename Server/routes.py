38d10e25d515eba35f0012a149bb877469d0e9a0
judybdma <jbdma23@outlook.com>
2023-03-29 16:30:55 -0300

    Changes

--- Server/routes.py
+++ Server/routes.py
@@ -1,9 +1,15 @@
 from flask import *
 from Server import app
 from Server.script import *
+import uuid
 
-user = "admin"
-password = "admin"
+
+secret_key = str(uuid.uuid4()).upper()
+secret_key = secret_key.replace("-", "")
+app.config['SECRET_KEY'] = secret_key[0:32]
+
+user = "admin@admin.com"
+password = "password"
 
 @app.route('/home')
 def home():
@@ -12,12 +18,14 @@ def home():
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        if request.form['inputEmail'] == user and request.form['inputPassword'] == password:
+        validation = checkLogin(request.form['inputEmail'], request.form['inputPassword'])
+        if validation:
             return redirect(url_for('home'))
         else:
-            return redirect(url_for('login'))
+            flash(u'Wrong credentials', category="error")
+            return render_template('login.html', form=request.form)
     else:
-        return render_template('login.html')
+        return render_template('login.html', form=request.form)
 
 
 @app.route('/book')
@@ -38,6 +46,18 @@ def profile():
 def shoppingCart():
     return render_template('ShoppingCart.html')
 
-@app.route('/sign-up')
+@app.route('/sign-up', methods=['GET', 'POST'])
 def signUp():
-    return render_template('Signup.html')
+    if request.method == 'POST':
+        if request.form['password'] == request.form['repeatPassword']:
+            createNewUser(request.form['name'], request.form['surname'], request.form['username'],
+                       request.form['inputEmail'], request.form['password'])
+
+            flash('User registered successfully!  Please log in.', category="usercreated")
+            return redirect(url_for('login'))
+        else:
+            flash('Password do not match')
+            return render_template('Signup.html', form=request.form)
+    else:
+        return render_template('Signup.html')
+
--- Server/script.py
+++ Server/script.py
@@ -1,3 +1,42 @@
+from Server import app
+import os
 import json
 import csv
 
+userdata  = os.path.join(app.static_folder, 'json', 'userdata.json')
+booksdata = os.path.join(app.static_folder, 'json', 'booksdata.json')
+
+def createUser(name, surname, username, email, password):
+    user = {"name": str(name),
+            "surname": str(surname),
+            "username": str(username),
+            "email": str(email),
+            "password": str(password)}
+
+    return user
+
+def readJSON(file):
+    with open(file, 'r') as openfile:
+        return json.load(openfile)
+
+def appendJSON(new_data, section, filename):
+    with open(filename, 'r+') as file:
+        file_data = json.load(file)
+        file_data[section].append(new_data)
+        file.seek(0)
+        json.dump(file_data, file, indent=4)
+
+def createNewUser(name, surname, username, email, password):
+    user = createUser(name, surname, username, email, password)
+    appendJSON(user, "userdata", userdata)
+
+def compareField(file, section, attribute, value):
+    file = readJSON(file)
+
+    for x in file[section]:
+        if x[attribute] == str(value):
+            return True
+    return False
+
+def checkLogin(email, password):
+    return compareField(userdata, "userdata", 'email', email) and compareField(userdata, 'userdata', 'password', password)
